917168b0e09f78a7dd707a1573782cf7dc60f832
nullmicgo <nullmicgo@nullmicgos-MacBook-Pro.local>
2019-01-11 18:04:07 +0800

    Adding Routes and  Morgan for logging purpose

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 
 var express = require("express");
 var app = express();
+var routes = require("./routes");
+
+var jsonParser = require("body-parser").json;
+var logger = require("morgan");
+
+app.use(logger("dev"));
+app.use(jsonParser());
+
+app.use("/questions", routes);
 
 var port = process.env.PORT ||3000;
 
